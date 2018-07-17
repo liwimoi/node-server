4182b248daeeed5e5a3452473c34e252ebb995c1
mth.bs.park <bbeomssoo@gmail.com>
2018-07-17 17:21:00 +0900

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var assert = require('assert');
-const {writeFileSync, readFileSync} = require('fs');
+const {writeFileSync, readFileSync, existsSync, mkdirSync} = require('fs');
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -29,7 +29,7 @@ const cassandraClient = new cassandra.Client({ contactPoints: ['localhost'], key
 
 
 producer.on('ready', function() {
-    console.log('producer is ready!');
+  console.log('producer is ready!');
 });
 
 consumer.on('message', function (message) {
@@ -38,70 +38,74 @@ consumer.on('message', function (message) {
 });
 
 app.get('/', function(req, res) {
-    res.sendFile( __dirname + "/" + "index.html");
+  res.sendFile( __dirname + "/" + "index.html");
 });
 
 app.post('/purchase', function(req, res) {
-        response = {
-                username: req.body.username,
-                amount: req.body.purchase_amount,
-        };
-        const record = [
-                {
-                        topic: "test",
-                        messages: JSON.stringify(response)
-                }
-        ];
-        producer.send(record, function(data, err) {
-                console.log(data);
-        });
+  response = {
+          username: req.body.username,
+          amount: req.body.purchase_amount,
+  };
+  const record = [
+          {
+                  topic: "test",
+                  messages: JSON.stringify(response)
+          }
+  ];
+  producer.send(record, function(data, err) {
+          console.log(data);
+  });
 
 
 
-        const query = 'INSERT INTO purchase (username, event_time, points) VALUES (?, ?, ?)';
-        const params = [req.body.username, new Date(), req.body.purchase_amount];
-        cassandraClient.execute(query, params, { prepare: true }, function(err) {
-          assert.ifError(err);
-        });
+  const query = 'INSERT INTO purchase (username, event_time, points) VALUES (?, ?, ?)';
+  const params = [req.body.username, new Date(), req.body.purchase_amount];
+  cassandraClient.execute(query, params, { prepare: true }, function(err) {
+    assert.ifError(err);
+  });
 
-        console.log(response);
-        res.send({
-          result: 200,
-        })
+  console.log(response);
+  res.send({
+    result: 200,
+  })
 });
 
 app.post('/list', function(req, res){
-        let result;
-        const {page, pageSize} = req.body;
-        const firstIdx = (page - 1) * pageSize;
-        try {
-                result  = JSON.parse(readFileSync('./tmp/list.json', 'utf8'));
-                result = result.splice(firstIdx, pageSize);
-        } catch (e) {
-                console.log('read file error..');
-                res.send({
-                  result: []
-                });
-        }
+  let result;
+  const {page, pageSize} = req.body;
+  const firstIdx = (page - 1) * pageSize;
+  try {
+          result  = JSON.parse(readFileSync('./tmp/list.json', 'utf8'));
+          result = result.splice(firstIdx, pageSize);
+  } catch (e) {
+          console.log('read file error..');
+          res.send({
+            result: []
+          });
+  }
 
-        res.send({
-          result: result
-        });
+  res.send({
+    result: result
+  });
 });
 
 app.get('/createList', function(req, res){
-        console.log('createJSONFile..');
-        let listItems = [];
-        for (let i = 0; i < 60; i++) {
-                listItems.push({
-                  id: i + 1
-                });
-        }
-        const data = JSON.stringify(listItems, null, '\t');
-        writeFileSync('./tmp/list.json', data);
-        res.send({
-          result: 'creat..'
-        });
+  console.log('createJSONFile..');
+  let listItems = [];
+  for (let i = 0; i < 60; i++) {
+          listItems.push({
+            id: i + 1
+          });
+  }
+  const data = JSON.stringify(listItems, null, '\t');
+
+  const existTmp = existsSync('./tmp');
+  if(!existTmp) mkdirSync('./tmp');
+
+  writeFileSync('./tmp/list.json', data);
+  res.send({
+    result: 'creat..'
+  });
 });
 
 app.listen(3000, function() {
