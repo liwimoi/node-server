ba01a9d2504a10c9518f671d10d92e5a243b84d7
liwimoi <liwimoi@liwimoiui-MacBook-Pro.local>
2018-07-17 15:35:04 +0900

    update drivers

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var app = express();
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var assert = require('assert')
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -7,11 +9,31 @@ app.use(bodyParser.urlencoded({ extended: true}));
 
 var kafka = require('kafka-node');
 var HighLevelProducer = kafka.HighLevelProducer;
-var client = new kafka.Client();
-var producer = new HighLevelProducer(client);
+var producerClient = new kafka.Client();
+var Consumer = kafka.Consumer;
+var consumerClient = new kafka.Client();
+var consumer = new Consumer(
+    consumerClient,
+    [
+        { topic: 'sum', partition: 0 }
+    ],
+    {
+        autoCommit: false
+    }
+);
+var producer = new HighLevelProducer(producerClient);
+
+const cassandra = require('cassandra-driver');
+const cassandraClient = new cassandra.Client({ contactPoints: ['localhost'], keyspace: 'test' });
+
 
 producer.on('ready', function() {
-        console.log('producer is ready!');
+    console.log('producer is ready!');
+});
+
+consumer.on('message', function (message) {
+  io.emit('refresh');
+  console.log(message);
 });
 
 app.get('/', function(req, res) {
@@ -25,19 +47,31 @@ app.post('/purchase', function(req, res) {
         };
         const record = [
                 {
-                        topic: "node-producer",
+                        topic: "test",
                         messages: JSON.stringify(response)
                 }
         ];
         producer.send(record, function(data, err) {
                 console.log(data);
         });
+
+
+
+        const query = 'INSERT INTO purchase (username, event_time, points) VALUES (?, ?, ?)';
+        const params = [req.body.username, new Date(), req.body.purchase_amount];
+        cassandraClient.execute(query, params, { prepare: true }, function(err) {
+          assert.ifError(err);
+        });
+
         console.log(response);
         res.redirect('/');
 });
+//
+// app.get('/top', function(req,res)) {
+//
+//   res.send()
+// }
 
-
-app.listen(3000, function() {
-        console.log('test app listening on port 3000!');
-});
-
+http.listen(3000,function() {
+  console.log('test app listening on port 3000!');
+})
